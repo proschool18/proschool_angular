ae594ef95b1f13589e62b21806220178da5304c4
proschool18 <66296318+proschool18@users.noreply.github.com>
2020-06-09 10:32:18 +0530

    Update students.module.ts

--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -5,6 +5,7 @@ import { SearchModuleModule } from '../search-module/search-module.module';
 import { TeacherSearchModule } from '../teacher-search/teacher-search.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { ClickOutsideDirective } from './_directives/click-outside.directive';
 
 import { StudentsRoutingModule } from './students-routing.module';
 import { InformationComponent } from './information/information.component';
@@ -12,7 +13,7 @@ import { AdmissionComponent } from './admission/admission.component';
 import { MatDatepickerModule, MatInputModule, MatNativeDateModule } from '@angular/material';
 
 @NgModule({
-  declarations: [InformationComponent, AdmissionComponent],
+  declarations: [InformationComponent, AdmissionComponent, ClickOutsideDirective],
   imports: [
     CommonModule,
     StudentsRoutingModule,
