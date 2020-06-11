bd671a5ec5b041586fc62be74224d9a92f89a814
amrutar <amruta.rasure@aegonlife.com>
2020-06-11 18:05:29 +0530

    added current day date and time on header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -10,8 +10,23 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+    today = new Date();
+	weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
   ngOnInit() {
 
   }
+
+	formatAMPM(date) {
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var ampm = hours >= 12 ? 'PM' : 'AM';
+		hours = hours % 12;
+		hours = hours ? hours : 12; // the hour '0' should be '12'
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		var strTime = hours + ':' + minutes + ' ' + ampm;
+		return strTime;
+	}
 
 }
